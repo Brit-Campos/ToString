1640b210328e8b2bb43066d7d34eaea1a2be4ddb
Brittanie Campos <brittanie.angelique@gmail.com>
2022-06-26 16:02:13 -0700

    Add project files.

---
+++ ToString/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // ToString() = converts an object to its string representation so it is suitable for display
+
+        Car car = new Car("Chevy", "Corvette", 2022, "blue");
+
+        Console.WriteLine(car.ToString());
+
+        Console.ReadKey();
+    }
+}
+
+class Car
+{
+    String make;
+    String model;
+    int year;
+    String color;
+
+    public Car(string make, string model, int year, string color)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public override string ToString()
+    {
+        return "This is a " + make + " " + model; ;
+    }
+}
